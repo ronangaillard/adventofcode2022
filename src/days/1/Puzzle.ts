590b807925dda902e180f3c905cc87216a6cabe1
Ronan Gaillard <ronan.gaillard@live.fr>
2022-12-01 10:45:03 +0100

    feat: solve day 1

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -2,16 +2,37 @@ import Puzzle from '../../types/AbstractPuzzle';
 
 export default class ConcretePuzzle extends Puzzle {
   public solveFirst(): string {
-    return 'day 1 solution 1';
+    const max = this.input
+      .split('\n\n')
+      .map((lines) =>
+        lines.split('\n').reduce((prev, current) => prev + parseInt(current), 0)
+      )
+      .filter((x) => x === x) // filters Nan (last array item)
+      .reduce((a, b) => Math.max(a, b), 0);
+
+    return max.toString();
   }
   public solveSecond(): string {
-    return 'day 1 solution 2';
+    const orderedCalories = this.input
+      .split('\n\n')
+      .map((lines) =>
+        lines.split('\n').reduce((prev, current) => prev + parseInt(current), 0)
+      )
+      .filter((x) => x === x) // filters Nan (last array item)
+      .sort((a, b) => b - a);
+
+    return (
+      orderedCalories[0] +
+      orderedCalories[1] +
+      orderedCalories[2]
+    ).toString();
   }
 
   public getFirstExpectedResult(): string {
-    return 'day 1 solution 1';
+    return '66616';
   }
+
   public getSecondExpectedResult(): string {
-    return 'day 1 solution 2';
+    return '199172';
   }
 }
